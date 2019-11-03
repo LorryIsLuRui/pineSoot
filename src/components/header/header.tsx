c8ed82dd39c616ec85067ecd6b7c0b67497e5fef
LorryIsLuRui <534774764@qq.com>
2019-11-03 21:28:21 +0800

    init

---
+++ src/components/header/header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './header.less';
+const HeaderCom:React.FC = ()=>{
+    return <div className="header-com-wrap">
+        <div className="logo"></div>
+        <div className="header-content">对对对</div>
+    </div>
+}
+export default HeaderCom;
---
+++ src/index/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HeaderCom from '../components/header/header';
+import './index.less';
+
+const IndexPage: React.FC = () => {
+    return (
+        <div className="home-page">
+            <HeaderCom />
+        </div>
+    );
+}
+
+export default IndexPage;
